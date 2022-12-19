2c284cf29f9ce9383e345f9931bc376b3109150a
devoarm <nattapongarm2017@gmail.com>
2022-12-19 11:05:08 +0700

    check have file

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const path = require("path");
 const hosOfficePath = "../../../documents/bookin";
 const auth = require("../middleware/auth");
-
+const fs = require("fs");
 const multer = require("multer");
 const upload = multer({ dest: "./public/data/uploads/" });
 require("dotenv").config();
@@ -14,24 +14,39 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/book-index/:name", function (req, res, next) {
-  var options = {
-    // root: path.join(__dirname, hosOfficePath),
-    root: DOCUMENT_PATH,
-    dotfiles: "deny",
-    headers: {
-      "x-timestamp": Date.now(),
-      "x-sent": true,
-    },
-  };
-
+router.get("/book-index/:name", async function (req, res, next) {
   var fileName = req.params.name;
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err);
-    } else {
+  var haveFilse = false;  
+  fs.readdirSync(path.join(__dirname, "../documents/bookin")).forEach(
+    (file) => {
+      console.log(`filename : ${fileName}`);
+      if (file === `${fileName}`) {
+        haveFilse = true;
+        console.log(`file : ${file}`);
+      }
     }
-  });
+  );
+  if (haveFilse) {
+    var options = {
+      root: path.join(__dirname, "../documents/bookin"),
+      // root: DOCUMENT_PATH,
+      dotfiles: "deny",
+      headers: {
+        "x-timestamp": Date.now(),
+        "x-sent": true,
+      },
+    };
+
+    res.sendFile(fileName, options, function (err) {
+      if (err) {
+        return res.json({ status: 500, msg: err.message });
+      } else {
+        return;
+      }
+    });
+  } else {
+    return res.json({ status: 301, msg: "no file" });
+  }
 });
 
 module.exports = router;
