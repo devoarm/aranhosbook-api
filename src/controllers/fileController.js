203e51bb5b400aa79fc1ef36bf5d9dd90d0d3d0c
devoarm <nattapongarm2017@gmail.com>
2022-12-19 15:21:20 +0700

    delete console

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -7,25 +7,23 @@ const DOCUMENT_PATH = process.env.DOCUMENT_PATH;
 
 const secret = process.env.SECRET_KEY;
 
-const CheckFile = async (req, res) => {  
-    var fileName = req.params.name;
-    var haveFilse = false;
-    try {
-      fs.readdirSync(DOCUMENT_PATH).forEach((file) => {
-        console.log(`filename : ${fileName}`);
-        if (file === `${fileName}`) {
-          haveFilse = true;
-          console.log(`file : ${file}`);
-        }
-      });
-      if (haveFilse) {
-        return res.json({ status: 200, msg: "has data" });
-      } else {
-        return res.json({ status: 301, msg: "no data" });
+const CheckFile = async (req, res) => {
+  var fileName = req.params.name;
+  var haveFilse = false;
+  try {
+    fs.readdirSync(DOCUMENT_PATH).forEach((file) => {
+      if (file === `${fileName}`) {
+        haveFilse = true;
       }
-    } catch (error) {
-      return res.json({ status: 500, msg: error.message });
+    });
+    if (haveFilse) {
+      return res.json({ status: 200, msg: "has data" });
+    } else {
+      return res.json({ status: 301, msg: "no data" });
     }
+  } catch (error) {
+    return res.json({ status: 500, msg: error.message });
+  }
 };
 
 module.exports = { CheckFile };
